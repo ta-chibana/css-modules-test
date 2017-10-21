50d9e58e8a62d08d64d97f73396719eeb5a7de0a
ta-chibana <tachibana.gmc@gmail.com>
2017-10-21 21:35:47 +0900

    Add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: './lib/index.js',
+    format: 'es'
+  },
+  external: ['react'],
+  plugins: [
+    typescript()
+  ]
+}
