b56c22784ebcfdecc72317621e2ad7e3c274b749
ta-chibana <tachibana.gmc@gmail.com>
2017-10-22 21:03:42 +0900

    Update webpack config to be able to use CSS Modules

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import * as styles from '../stylesheets/index.css'
+import * as other from '../stylesheets/other.css'
 
 interface IArticleProps {
   title: string
@@ -6,7 +8,7 @@ interface IArticleProps {
 
 const Article = (props: IArticleProps) => {
   return (
-    <div>
+    <div className={`${styles.pageTitle} ${other.titleSize}`}>
       {props.title}
     </div>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'examples/src/index.tsx'),
@@ -18,9 +19,19 @@ module.exports = {
         use: [
           { loader: 'awesome-typescript-loader' }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader?modules"
+        })
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin("./styles.css")
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, 'examples/dist/')
   }
